eded5512d7482baed085fdb4226a80df6c4492a4
oluciams <oluciams84@gmail.com>
2023-05-04 16:20:27 -0500

    add card component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
+import Card from "../../components/Card";
 import Layout from "../../components/Layout";
 
 function Home() {
   return (
     <Layout>
       Home
+      <Card />
     </Layout>
   )
 }
