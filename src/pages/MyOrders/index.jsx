d6a298f04584effafca4240a1122f048ba2f8cf9
oluciams <oluciams84@gmail.com>
2023-05-04 11:37:03 -0500

    add initial configuration, folder structure and install tailwind

---
+++ src/pages/MyOrders/index.jsx
@@ -0,0 +1,5 @@
+function MyOrders() {
+  return <div className="bg-red-100">My Orders</div>;
+}
+
+export default MyOrders;
---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,5 @@
+function NotFound() {
+  return <div className="bg-red-100">Not Found</div>;
+}
+
+export default NotFound;
